1acca6af943c6cbe6ac3c0e705faca226f51777f
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-06-02 20:20:46 -0700

    Navbar css cleanup

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,33 +1,34 @@
 import { NavLink, Redirect } from "react-router-dom";
 import "./Navbar.css"
 import UserContext from "./UserContext";
-import {useContext} from "react";
+import { useContext } from "react";
 
 
 function Navbar({ logout }) {
 
-  const {currentUser} = useContext(UserContext)
+  const { currentUser } = useContext(UserContext)
 
   return (
     <nav className="Navbar-nav">
-      <ul className="Navbar-list">
-        <li className="Navbar-listitem"><NavLink exact to="/">Jobly </NavLink></li>
-        {currentUser && (
-          <>
-            <li className="Navbar-listitem"><NavLink exact to="/companies">Companies</NavLink></li>
-            <li className="Navbar-listitem"><NavLink exact to="/jobs">Jobs</NavLink></li>
-            <li className="Navbar-listitem"><NavLink exact to="/profile">Profile</NavLink></li>
-            <li className="Navbar-listitem"><NavLink exact to="/" onClick={logout}>Logout</NavLink></li>
-          </>
-        )
-        }
-        {!currentUser && (
-          <>
-            <li className="Navbar-listitem"><NavLink exact to="/login">Login</NavLink></li>
-            <li className="Navbar-listitem"><NavLink exact to="/signup">Signup</NavLink></li>
-          </>
-        )}
-      </ul>
+      <NavLink className="Navbar-listitem Navbar-name" exact to="/">Jobly </NavLink>
+        <ul className="Navbar-list">
+          {currentUser && (
+            <>
+              <li className="Navbar-listitem"><NavLink exact to="/companies">Companies</NavLink></li>
+              <li className="Navbar-listitem"><NavLink exact to="/jobs">Jobs</NavLink></li>
+              <li className="Navbar-listitem"><NavLink exact to="/profile">Profile</NavLink></li>
+              <li className="Navbar-listitem"><NavLink exact to="/" onClick={logout}>Logout</NavLink></li>
+            </>
+          )
+          }
+          {!currentUser && (
+            <>
+              <li className="Navbar-listitem"><NavLink exact to="/login">Login</NavLink></li>
+              <li className="Navbar-listitem"><NavLink exact to="/signup">Signup</NavLink></li>
+            </>
+          )}
+        </ul>
+
     </nav>
   )
 }
